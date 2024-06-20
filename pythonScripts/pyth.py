801e43d52645e2b2f4f48f634a10fd2591c80ea7
Vahl <vahl@kali.kali>
2024-06-20 08:19:26 -0400

    This is my first pythonscript

---
+++ pythonScripts/pyth.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+num1 = 2
+num2 = 8
+sum = num1 + num2
+print(sum)
+
+num3 = 10
+num4 = 5
+sum = num3 + num4
+print(type(sum))
+
+   
+x, y, z = 'val', 'en', 'tine'
+print(x)
+print(y)
+print(z)
+
+a = b = c = "vahlentine"
+print(a)
+print(b)
+print(c)
+
+fruits = ( "banana", "apple", "cashew")
+o, p, q = fruits
+print(o)
+print(p)
+print(q)
