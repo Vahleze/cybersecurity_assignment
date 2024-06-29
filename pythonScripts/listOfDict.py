09da5b4605a1743335dc5194a1b4176da3da8ee7
Vahl <vahl@kali.kali>
2024-06-29 15:14:29 -0400

    Added list of dictionary

---
+++ pythonScripts/listOfDict.py
@@ -0,0 +1,144 @@
+#!/usr/bin/python
+
+mydict = [
+        {
+            "id": 1,
+            "firstname": "Chiamanda",
+            "lastname": "Chude",
+            "email": "amanda20202@gamil.com",
+            "phonenumber": "08029384765",
+            "Isactive": 1,
+            "faculty": "NaturalScience",
+            "courses": [ "Cell_Biology", "General_Chemistry", "Practical_Chemistry", "Mathematics_Algebra"],
+            "department": "Biochemistry",
+            "regNo": "EBSU/24/34003"
+        },
+        {
+            "id": 2,
+            "firstname": "Emeka",
+            "lastname": "Okoro",
+            "email": "mekusokoroman@gmail.com",
+            "phonenumber": "09034563764",
+            "Isactive": 1,
+            "faculty": "PhysicalScience",
+            "courses": ["IntroductionToComputers", "HistoryOfComputers", "HTMl", "Mathematics_Algebra"],
+            "department": "Computer_Science",
+            "regNo": "EBSU/24/48933"
+        },
+        {
+            "id": 3,
+            "firstname": "Odinaka",
+            "lastname": "Ifesinachi",
+            "email": "odiann@hotmail.com",
+            "phonenumber": "08103887680",
+            "Isactive": 1,
+            "faculty": "PhysicalScience",
+            "courses": ["IntroductionToComputers", "HistoryOfComputers", "HTMl", "Mathematics_Algebra"],
+            "department": "Computer_Science",
+            "regNo": "EBSU/24/34998"
+         },
+        {
+            "id": 4,
+            "firstname": "Benson",
+            "lastname": "Chinkata",
+            "email": "katakata05@gamil.com",
+            "phonenumber": "08100027632",
+            "Isactive": 1,
+            "faculty": "PhysicalScience",
+            "courses": ["HistoryOfComputers", "Mathematics_Algebra", "Geometry", "IntroductionToGoelogy"],
+            "department": "Geology",
+            "regNo": "EBSU/24/89398"
+         },
+           {
+               "id": 5,
+               "firstname": "Tobez",
+               "lastname": "Amajuoyi",
+               "email": "Tobezamah@gmail.com",
+               "phonenumber": "08093284611",
+               "Isactive": 1,
+               "faculty": "HealthScience",
+               "courses": ["General_Biology", "General_Chemistry",  "Mathematics_Alagebra", "Practical_Chemistry"],
+               "department": "Medicine",
+               "regNo": "EBSU/24/77640"
+             },
+           {
+                   "id": 6,
+                   "firstname": "Anene",
+                   "lastname": "Chibueze",
+                   "email": "Anene00@gmail.com",
+                   "phonenumber": "07026712134",
+                   "Isactive": 1,
+                   "faculty": "HealthScience",
+                   "courses": ["General_Biology", "General_Chemistry", "Mathematics_Algebra", "Practical_Chemistry"],
+                   "department": "Medicine",
+                   "regNo": "EBSU/24/76293"
+                   },
+           {
+                   "id": 7,
+                   "firstname": "Lawrence",
+                   "lastname": "Ezeofor",
+                   "email": "greatlaw@gmail.com",
+                   "phonenumber": "09031221289",
+                   "Isactive": 1,
+                   "faculty": "HealthScience",
+                   "courses": ["General_Biology", "General_Chemistry", "Practical_Chemistry", "Physics"],
+                   "department": "Nursing",
+                   "regNo": "EBSU/24/78393"
+                   },
+           {
+                   "id": 8,
+                   "firstname": "Jude",
+                   "lastname": "Udah",
+                   "email": "udahjuddie@gmail.com",
+                   "phonenumber": "08097649875",
+                   "Isactive": 1,
+                   "faculty": "PhysicalScience",
+                   "courses": ["General_Mathematics", "Mathematics_Algebra", "Physics", "HTML"],
+                   "department": "Mathematics",
+                   "regNo": "EBSU/24/66331"
+                   },
+           {
+                   "id": 9,
+                   "firstname": "Chinenye",
+                   "lastname": "Anyanwu",
+                   "email": "anyayanenye@gmail.com",
+                   "phonenumber": "08103223432",
+                   "Isactive": 1,
+                   "faculty": "HealthScience",
+                   "courses": ["General_Biology", "General_Chemistry", "Practical_Chemistry", "Physics"],
+                   "department": "Nursing",
+                   "regNo": "EBSU/24/71212"
+                   },
+           {
+                   "id": 10,
+                   "firstname": "Ken",
+                   "lastname": "Erics",
+                   "email": "kennyerics@gmail.com",
+                   "phonenumber": "07054325672",
+                   "Isactive": 1,
+                   "faculty": "NaturalScience",
+                   "courses": ["Cell_Biology", "General_Chemistry", "Practical_Chemistry", "Mathematics_Algebra"],
+                   "department": "Biochemistry",
+                   "regNo": "EBSU/24/34982"
+                   },
+           {
+                   "id": 11,
+                   "fisrtname": "Micah",
+                   "lastname": "Davis",
+                   "email": "davismicah1010@gmail.com",
+                   "phonenumber": "09091123432",
+                   "Isactive": 1,
+                   "faculty": "NaturalScience",
+                   "courses": ["Cell_Biology", "General_Chemistry", "Practical_Chemistry", "Mathematics_Algebra"],
+                   "department": "Biochemistry",
+                   "regNo": "EBSU/24/34996"
+                   }
+
+        
+
+
+
+
+        ]
+
+print(mydict)
